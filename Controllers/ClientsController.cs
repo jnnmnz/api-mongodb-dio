283aeb48981dc61e1a2cd3355b950c7f201121b6
jnnmnz <jenifercarvalho_@hotmail.com>
2021-08-07 21:02:45 -0300

    fixed bd connection and added commands DELETE and PATCH

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using ApiEscolaridade.Data.Collections;
+using ApiEscolaridade.Models;
+using MongoDB.Driver;
 
 namespace ApiEscolaridade.Controllers
 {
@@ -6,6 +10,48 @@ namespace ApiEscolaridade.Controllers
     [Route("controller")]
     public class ClientsController : ControllerBase
     {
-        
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Client> _clientCollection;
+
+        public ClientsController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _clientCollection = _mongoDB.DB.GetCollection<Client>(typeof(Client).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult AddClient([FromBody] ClientDTO dto)
+        {
+            var client = new Client(dto.Id, dto.BirthYear, dto.Gender, dto.EducLevel, dto.BasicGradYear);
+
+            _clientCollection.InsertOne(client);
+            
+            return StatusCode(201, "CLIENT ADDED SUCCESSFULLY");
+        }
+
+        [HttpGet]
+
+        public ActionResult GetClient()
+        {
+            var client = _clientCollection.Find(Builders<Client>.Filter.Empty).ToList();
+            
+            return Ok(client);
+        }
+
+        [HttpPatch]
+        public ActionResult UpdateClient([FromBody] ClientDTO dto)
+        {
+            _clientCollection.UpdateOne(Builders<Client>.Filter.Where(_ => _.Id == dto.Id), Builders<Client>.Update.Set("gender", dto.Gender));
+            
+            return Ok("PERSON DATA UPDATED SUCCESSFULLY");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteClient(int id)
+        {
+            _clientCollection.DeleteOne(Builders<Client>.Filter.Where(_ => _.Id == id));
+                        
+            return Ok("PERSON REMOVED SUCCESSFULLY");
+        }
     }
 }
--- Data/Collections/Client.cs
+++ Data/Collections/Client.cs
@@ -2,14 +2,16 @@ namespace ApiEscolaridade.Data.Collections
 {
     public class Client
     {
-        public Client(int birthYear, string gender, string educLevel, int basicGradYear)
+        public Client(int id, int birthYear, string gender, string educLevel, int basicGradYear)
         {
+            this.Id = id;
             this.BirthYear = birthYear;
             this.Gender = gender;
             this.EducLevel = educLevel;
             this.BasicGradYear = basicGradYear;
         }
         
+        public int Id { get; set; }
         public int BirthYear { get; set; }
         public string Gender { get; set; }
         public string EducLevel { get; set; }
